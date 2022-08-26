8b12528a23d40ecb2cc4c14dd51f4edfcc539553
Arsen Tamazyan <atamazyan@tsconsulting.com>
2022-08-26 08:57:28 +0300

    init tsc project

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Test = void 0;
+class Test {
+    constructor(handler) {
+        console.log(handler);
+        const t = handler;
+        console.log(t);
+    }
+}
+exports.Test = Test;
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+export class Test {
+    constructor(handler: any) {
+        console.log(handler);
+        const t = handler;
+        console.log(t)
+    }
+}
