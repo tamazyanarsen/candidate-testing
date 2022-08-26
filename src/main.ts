7c9c41b644592792c07637ecc7bfc3784a19c71c
Arsen Tamazyan <atamazyan@tsconsulting.com>
2022-08-26 11:16:55 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,17 @@
-const rootElement = document.getElementById('root');
+const codeText = document.querySelector('.code-text') as HTMLDivElement;
+const sendBtn = document.querySelector('.btn-send');
+const testCode = `
+const newP = new MyPromise(resolve => setTimeout(() => {
+    resolve(12)
+}, 500));
 
-console.log(rootElement);
+newP.then(e => console.log(e + 1)) // 13
+    .then(e => console.log(e + 1)); // 14
+
+newP.then(e => console.log(e + 1)); // 13
+`;
+const testCodeElement = document.querySelector('.test-code');
+if (testCodeElement) testCodeElement.innerHTML = `<pre>${testCode}</pre>`;
+sendBtn?.addEventListener('click', () => {
+    eval([codeText.innerText, testCode].join('\n'));
+});
